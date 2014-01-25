057666361bbf4071e38da273115473936074bcec
Radoslav Stankov <rstankov@gmail.com>
2014-01-25 13:52:41 +0200

    Play with grid

--- BachkovoTheTurnaround/BTTMapScene.m
+++ BachkovoTheTurnaround/BTTMapScene.m
@@ -22,6 +22,33 @@
         
         NSLog(@"%f", myLabel.position.y);
         
+
+       
+        NSInteger size = 44;
+        for(NSInteger i=0; i<30; i++) {
+            for(NSInteger j=0; j<40; j++) {
+                SKSpriteNode *sprite = [[SKSpriteNode alloc] initWithImageNamed:@"square"];
+                
+                sprite.position = CGPointMake(22 + size * i, CGRectGetMidY(self.frame) + 68 - size * j);
+                
+                NSLog(@"%@", NSStringFromCGSize(sprite.size));
+                
+                sprite.color = [SKColor colorWithRed:(1.0 * i)/30.0 green:(1.0 * (arc4random() % 74)) / 74 blue:(1.0*j)/40.0 alpha:2.0];
+                sprite.colorBlendFactor = 0.3;
+            
+                SKLabelNode *label = [SKLabelNode labelNodeWithFontNamed:@"HelveticaLight"];
+                
+                label.text = [NSString stringWithFormat:@"%dx%d", i, j];
+                label.fontSize = 10;
+                label.position = CGPointMake(0, 0);
+                
+                [sprite addChild:label];
+                
+                [self addChild:sprite];
+            }
+        }
+        
+        
         [self addChild:myLabel];
     }
     return self;
