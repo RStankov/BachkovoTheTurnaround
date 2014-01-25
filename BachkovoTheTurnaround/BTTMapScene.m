c4da526e1f5ad544d5b26e96cc6c37dbc6cedd00
Radoslav Stankov <rstankov@gmail.com>
2014-01-25 12:14:39 +0200

    Hide status bar

---
+++ BachkovoTheTurnaround/BTTMapScene.m
@@ -0,0 +1,13 @@
+//
+//  BTTMapScene.m
+//  BachkovoTheTurnaround
+//
+//  Created by Radoslav Stankov on 1/25/14.
+//  Copyright (c) 2014 Radoslav Stankov. All rights reserved.
+//
+
+#import "BTTMapScene.h"
+
+@implementation BTTMapScene
+
+@end
