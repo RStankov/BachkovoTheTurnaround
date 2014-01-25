923ad919892c8a52a0bfc0180783277ded763ad1
Stefan Slaveykov <slaveykov@fidor.de>
2014-01-25 12:14:31 +0200

    Mech garrison building

---
+++ BachkovoTheTurnaround/BTTMechGarrisonBuilding.h
@@ -0,0 +1,14 @@
+//
+//  BTTMechGarrisonBuilding.h
+//  BachkovoTheTurnaround
+//
+//  Created by Stefan Slaveykov on 25/01/14.
+//  Copyright (c) 2014 Radoslav Stankov. All rights reserved.
+//
+
+#import "BTTBuilding.h"
+
+@interface BTTMechGarrisonBuilding : BTTBuilding
+@property(nonatomic, assign) NSMutableArray *units;
+
+@end
---
+++ BachkovoTheTurnaround/BTTMechGarrisonBuilding.m
@@ -0,0 +1,13 @@
+//
+//  BTTMechGarrisonBuilding.m
+//  BachkovoTheTurnaround
+//
+//  Created by Stefan Slaveykov on 25/01/14.
+//  Copyright (c) 2014 Radoslav Stankov. All rights reserved.
+//
+
+#import "BTTMechGarrisonBuilding.h"
+
+@implementation BTTMechGarrisonBuilding
+
+@end
