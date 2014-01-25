7d118ecff0b0c8f54293c6fa6cbdb461485d3f70
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2014-01-25 14:06:48 +0200

    City scene added

--- BachkovoTheTurnaround/BTTMyCity.m
+++ BachkovoTheTurnaround/BTTMyCity.m
@@ -12,14 +12,43 @@
 @implementation BTTMyCity
 
 -(id)initWithSize:(CGSize)size {
-    if(self = [super initWithSize:size]) {
-        self.backgroundColor = [SKColor colorWithRed:0.10 green:0.5 blue:0.10 alpha:0.5];
-        SKNode *mainBuilding = [[BTTBuilding alloc] init];
-
-        mainBuilding.position = CGPointMake(CGRectGetMidX(self.frame),
-                                       CGRectGetMidY(self.frame));
-        
-        [self addChild:mainBuilding];
+    if (self = [super initWithSize:size]) {
+        self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
+       
+       SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"HelveticaLight"];
+       
+       myLabel.text = @"City";
+       myLabel.fontSize = 10;
+       myLabel.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame) + 80);
+       
+       NSLog(@"%f", myLabel.position.y);
+       
+       NSInteger size = 44;
+       for(NSInteger i=0; i<30; i++) {
+           for(NSInteger j=0; j<40; j++) {
+               SKSpriteNode *sprite = [[SKSpriteNode alloc] initWithImageNamed:@"square"];
+               
+               sprite.position = CGPointMake(22 + size * i, CGRectGetMidY(self.frame) + 68 - size * j);
+               
+               NSLog(@"%@", NSStringFromCGSize(sprite.size));
+               
+               sprite.color = [SKColor colorWithRed:(1.0 * i)/30.0 green:(1.0 * (arc4random() % 74)) / 74 blue:(1.0*j)/40.0 alpha:2.0];
+               sprite.colorBlendFactor = 0.3;
+               
+               SKLabelNode *label = [SKLabelNode labelNodeWithFontNamed:@"HelveticaLight"];
+               
+               label.text = [NSString stringWithFormat:@"%dx%d", i, j];
+               label.fontSize = 10;
+               label.position = CGPointMake(0, 0);
+               
+               //[sprite addChild:label];
+               
+               [self addChild:sprite];
+           }
+       }
+       
+       
+       [self addChild:myLabel];
     }
     return self;
 }
