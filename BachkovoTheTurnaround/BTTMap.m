3c6e986ff9f6ed4127108e0f639fbe767d4616de
Radoslav Stankov <rstankov@gmail.com>
2014-01-26 02:49:24 +0200

    Increase tunnel size

--- BachkovoTheTurnaround/BTTMap.m
+++ BachkovoTheTurnaround/BTTMap.m
@@ -28,7 +28,7 @@
           @[o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o],
           @[o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o],
           @[o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, o, _, _, _, _, o, o, o, o, o, _, o, o, o, o, o],
-          @[o, o, o, o, o, _, _, _, _, _, o, o, o, o, o, o, o, o, _, _, _, o, o, o, _, _, _, _, _, _, o, o, o, _, _, o, o, o, o, o],
+          @[o, o, o, o, o, _, _, _, _, _, o, o, o, o, o, o, o, o, _, _, _, _, _, _, _, _, _, _, _, _, o, o, o, _, _, o, o, o, o, o],
           @[o, o, o, o, o, _, _, _, _, _, o, o, o, o, o, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, o, o, _, _, o, o, o, o, o],
           @[o, o, o, o, _, _, _, _, _, _, o, o, o, o, o, _, _, _, _, _, _, _, o, o, o, _, _, _, _, _, _, o, _, _, o, o, o, o, o, o],
           @[o, o, o, o, _, _, _, _, _, _, _, o, o, o, o, o, _, _, _, o, o, o, o, o, o, o, _, _, _, _, _, o, _, o, o, o, o, o, o, o],
