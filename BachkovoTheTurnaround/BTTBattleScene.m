b42eb0062c36c9f4ec55586628fcf4890683a278
Radoslav Stankov <rstankov@gmail.com>
2014-01-26 09:10:04 +0200

    Show units on the battleground

--- BachkovoTheTurnaround/BTTBattleScene.m
+++ BachkovoTheTurnaround/BTTBattleScene.m
@@ -24,10 +24,42 @@
         
         SKSpriteNode *background  = [SKSpriteNode spriteNodeWithImageNamed:@"battleground"];
         background.anchorPoint = CGPointMake(0, 1);
-        
         background.position = CGPointMake(0, 0);
         
         [self addChild:background];
+        
+        
+        SKSpriteNode *card;
+        
+        card = [SKSpriteNode spriteNodeWithImageNamed:@"unit_1"];
+        card.anchorPoint = CGPointMake(0, 1);
+        card.position = CGPointMake(44 + 99 * 0, -11);
+
+        [self addChild:card];
+        
+        card = [SKSpriteNode spriteNodeWithImageNamed:@"unit_2"];
+        card.anchorPoint = CGPointMake(0, 1);
+        card.position = CGPointMake(44 + 99 * 1, -11);
+
+        [self addChild:card];
+
+        card = [SKSpriteNode spriteNodeWithImageNamed:@"unit_3"];
+        card.anchorPoint = CGPointMake(0, 1);
+        card.position = CGPointMake(44 + 99 * 2, -11);
+
+        [self addChild:card];
+
+        card = [SKSpriteNode spriteNodeWithImageNamed:@"unit_hidden"];
+        card.anchorPoint = CGPointMake(0, 1);
+        card.position = CGPointMake(44 + 99 * 3, -11);
+
+        [self addChild:card];
+
+        card = [SKSpriteNode spriteNodeWithImageNamed:@"unit_1"];
+        card.anchorPoint = CGPointMake(0, 1);
+        card.position = CGPointMake(44 + 99 * 0, -11 - 204.5);
+
+        [self addChild:card];
 
         enemyArmyBar = [[BTTArmyBar alloc] initWithSlotsCount:6 units:@[]];
         enemyArmyBar.position = CGPointMake(CGRectGetMidX(self.frame), self.frame.size.height - enemyArmyBar.size.height / 2);
